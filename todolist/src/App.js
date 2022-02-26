9c842adec76decc748ab81d6648ef729c0b71f49
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-26 21:32:39 +0900

    Feat: add Todo.js

---
+++ todolist/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import TodoList from "./TodoList";
+
+function App() {
+  const [todos, setTodos] = useState([
+    { id: 1, name: "Todo 1", complete: false },
+  ]);
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  );
+}
+
+export default App;
