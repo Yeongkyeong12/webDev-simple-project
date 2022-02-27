cfe0cd88eaa99f19d73447b4687aba0b5957c37c
Yeongkyeong12 <ohkyeong1995@gmail.com>
2022-02-27 23:18:10 +0900

    Feat: add another useEffect

--- todolist/src/Todo.js
+++ todolist/src/Todo.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function Todo({ todo }) {
-  return <div>{todo.name}</div>;
+export default function Todo({ todo, toggleTodo }) {
+  function handleTodoClick() {
+    toggleTodo(todo.id);
+  }
+  return (
+    <div>
+      <label>
+        <input
+          type="checkbox"
+          checked={todo.complete}
+          onChange={handleTodoClick}
+        />
+        {todo.name}
+      </label>
+    </div>
+  );
 }
